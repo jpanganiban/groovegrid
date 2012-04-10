2439775ec9e84ab6f0fca98d0c3c3b0ee45ad35a
Jesse Panganiban <me@jpanganiban.com>
2012-04-11 01:28:12 +0800

    Added models

--- groovegrid.py
+++ groovegrid.py
@@ -7,6 +7,25 @@ app.config['MONGOALCHEMY_DATABASE'] = 'groovegrid_dev3'
 db = MongoAlchemy(app)
 
 
+class Grid(db.Document):
+  name = db.StringField()
+
+  def to_dict(self):
+    return {'id': str(self.mongo_id),
+            'name': self.name,
+           }
+
+
+class Tile(db.Document):
+  grid_id = db.ObjectIdField()
+  title = db.StringField()
+
+  def to_dict(self):
+    return {'id': str(self.mongo_id),
+            'title': self.title,
+           }
+
+
 @app.route('/')
 @app.route('/<grid_name>')
 def index(grid_name=None):
