2e8de8e43c758ed1c643d7890c2894e31f3f487c
Jesse Panganiban <me@jpanganiban.com>
2012-06-05 17:05:39 +0800

    Moved index to its own module

--- groovegrid/server.py
+++ groovegrid/server.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
-import gevent
+from flask import Flask
+from groovegrid.controllers import index
+
 
 app = Flask(__name__, template_folder='views')
+# Register blueprints
+app.register_blueprint(index.app)
 
-
-@app.route('/')
-def index():
-  return 'Groovegrid'
 
 def debug(app=app, host='0.0.0.0', port=51000):
+  """Console script entry point for 'groovegrid.debug' command."""
   app.run(debug=True, host=host, port=port)
