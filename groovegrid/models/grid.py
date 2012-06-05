bbe056949b04b6ac18ebb6eba697de802097ef8c
Jesse Panganiban <me@jpanganiban.com>
2012-06-05 21:48:21 +0800

    Added grid-tile relationship

--- groovegrid/models/grid.py
+++ groovegrid/models/grid.py
@@ -4,8 +4,10 @@ from groovegrid.models import db
 class Grid(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128))
+  tiles = db.relationship('Tile', backref='grid')
 
 
 class Tile(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  grid_id = db.Column(db.Integer, db.ForeignKey('grid.id'))
   name = db.Column(db.String(256))
