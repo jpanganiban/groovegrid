d01423f310483e69b05d2feb5985e9d2ac8c4979
Jesse Panganiban <me@jpanganiban.com>
2012-06-05 18:27:00 +0800

    Added sqlalchemy instance

--- groovegrid/server.py
+++ groovegrid/server.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from groovegrid.models import db
 from groovegrid.controllers import index
 
 
 app = Flask(__name__, template_folder='views')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///groovegrid.db'
+# Attach db
+db.init_app(app)
 # Register blueprints
 app.register_blueprint(index.app)
 
